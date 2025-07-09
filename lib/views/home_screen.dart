f7ce93bae9b9b88e75365282dd905f57b2fe3614
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-09 21:30:32 +0530

    dice app complete

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,12 +1,20 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
 
   @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int imagenumber = Random().nextInt(5) + 1;
+  // Random rand = Random();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(100, 208, 208, 208),
+      backgroundColor: Color.fromARGB(99, 115, 110, 110),
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.symmetric(vertical: 32.0),
@@ -15,13 +23,28 @@ class MyWidget extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                "Dice Roll App",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                ("Dice Roll App"),
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w800,
+                  color: const Color.fromARGB(255, 13, 11, 11).withAlpha(105),
+                ),
               ),
-              Image.asset(''),
+              Image.asset('images/dice-$imagenumber.png'),
               ElevatedButton(
-                onPressed: null,
-                child: Text("Roll Dice", style: TextStyle(fontSize: 25)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black38,
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+                ),
+                onPressed: () {
+                  setState(() {
+                    imagenumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Text(
+                  ("Roll Dice").toUpperCase(),
+                  style: TextStyle(color: Color.fromARGB(255, 245, 245, 245)),
+                ),
               ),
             ],
           ),
